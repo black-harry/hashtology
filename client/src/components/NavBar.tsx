3e878c050442c05753384a9aea58d66ae1f999a5
JasperJac <c38338490@gmail.com>
2022-01-22 00:00:17 -0500

    building NavBar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,5 +1,6 @@
-import { HiMenuAlt4 } from 'react-icons/hi';
+import { HiMenuAlt1 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
 
 interface Props {
   title: string;
@@ -12,8 +13,9 @@ const NavbarItem = ({ title, classProps }: Props) => {
 
 const NavBar = () => {
   const NavArr = ['Market', 'Swap Token', 'Exchange', 'Wallets'];
+  const [toggleMenu, setToggleMenu] = useState<boolean>(false);
   return (
-    <nav className='w-full flex md:justify-center justify-between p-4'>
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
       <div className='md:flex-[0.5] flex-initial justify-center items-center'>
         <img
           src='../../images/hashtology-logo-2.png'
@@ -21,11 +23,36 @@ const NavBar = () => {
           className=' w-36 cursor-pointer'
         />
       </div>
-      <ul className='text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial'>
+      <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
         {NavArr.map((item, index) => (
           <NavbarItem key={index + item} title={item} classProps={''} />
         ))}
       </ul>
+      <div className='flex relative'>
+        {toggleMenu ? (
+          <AiOutlineClose
+            fontSize={28}
+            className='text-white md:hidden cursor-pointer'
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <HiMenuAlt1
+            fontSize={28}
+            className='text-white md:hidden cursor-pointer'
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <ul className='z-10 fixed top-0 right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end text-gray-400'>
+            <li className='text-xl w-full my-2'>
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+              {NavArr.map((item, index) => (
+                <NavbarItem key={index + item} title={item} classProps={''} />
+              ))}
+            </li>
+          </ul>
+        )}
+      </div>
     </nav>
   );
 };
--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,5 +1,5 @@
 const Welcome = () => {
-  return <h1>Welcome</h1>;
+  return <h1 className=''>Welcome</h1>;
 };
 
 export default Welcome;
