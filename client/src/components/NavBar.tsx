47ec4bee5aecb17d28ba4e11b413cc9ccbb53fb1
JasperJac <c38338490@gmail.com>
2022-01-22 18:39:01 -0500

    edit NavNar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -26,7 +26,7 @@ const NavBar = () => {
         <img
           src='../../images/hashtology-logo-2.png'
           alt='logo'
-          className=' w-[12rem] cursor-pointer'
+          className=' md:w-[12rem] w-[15rem]  cursor-pointer'
         />
       </div>
       <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
