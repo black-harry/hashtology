bce33e7284886c31fad9c538f91cacdc1a7fc262
JasperJac <c38338490@gmail.com>
2022-07-19 17:52:09 -0500

    Removed unused props

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -10,7 +10,6 @@ interface Props {
   placeholder: string;
   name: string;
   type: string;
-  // value: string;
   handleChange: (e: any, name: string) => void;
 }
 
@@ -28,7 +27,6 @@ const Input = ({
       placeholder={placeholder}
       type={type}
       step='0.0001'
-      // value={value}
       onChange={(e) => handleChange(e, name)}
       className='my-2 w-full rounded-md p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
     />
